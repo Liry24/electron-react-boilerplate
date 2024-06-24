67a779ab3c9d5fc894cfc60e0d752c58a20ac9f2
Liry24 <liry241999@gmail.com>
2024-06-25 06:01:16 +0900

    markdown

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,132 @@
+import { useState, useEffect } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+// データ型を定義
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<HomeScreen />} />
       </Routes>
     </Router>
   );
 }
+
+const HomeScreen = () => {
+  // stateを定義
+  const [text, setText] = useState<string>('');
+  const [todoList, setTodoList] = useState<Array<Todo>>([]);
+
+  useEffect(() => {
+    // 初回レンダー時にデフォルトのデータをセット
+    const defaultTodoList = [
+      {
+        id: 1,
+        text: 'ばーか',
+        completed: false,
+      },
+      {
+        id: 2,
+        text: 'あーほ',
+        completed: true,
+      },
+      {
+        id: 3,
+        text: 'おしりぷり音頭',
+        completed: false,
+      },
+    ];
+
+    setTodoList(defaultTodoList);
+  }, []);
+
+  const onSubmit = () => {
+    // ボタンクリック時にtodoListに新しいToDoを追加
+    if (text !== '') {
+      const newTodoList: Array<Todo> = [
+        {
+          id: new Date().getTime(),
+          text: text,
+          completed: false,
+        },
+        ...todoList,
+      ];
+      setTodoList(newTodoList);
+
+      // テキストフィールドを空にする
+      setText('');
+    }
+  };
+
+  const onCheck = (newTodo: Todo) => {
+    // チェック時にcompletedの値を書き換える
+    const newTodoList = todoList.map((todo) => {
+      return todo.id == newTodo.id
+        ? { ...newTodo, completed: !newTodo.completed }
+        : todo;
+    });
+    setTodoList(newTodoList);
+  };
+
+  const markdownString = `
+  # headind
+
+  - [ ] checkbox
+  - [x] checked checkbox
+  `;
+
+  return (
+    <div>
+      <div className="container">
+        <div className="input-field">
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={onSubmit} className="add-todo-button">
+            Add
+          </button>
+        </div>
+
+        <ul className="todo-list">
+          {todoList?.map((todo) => {
+            return <Todo key={todo.id} todo={todo} onCheck={onCheck} />;
+          })}
+        </ul>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {markdownString}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+const Todo = (props: { todo: Todo; onCheck: Function }) => {
+  const { todo, onCheck } = props;
+  const onCheckHandler = () => {
+    onCheck(todo);
+  };
+  return (
+    <li className={todo.completed ? 'checked' : ''}>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={onCheckHandler}
+        ></input>
+        <span>{todo.text}</span>
+      </label>
+    </li>
+  );
+};
